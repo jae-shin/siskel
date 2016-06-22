72ec100c57672ffc6518975717ec7f15e03740cc
Jae Shin <jae5464@gmail.com>
2016-06-22 14:24:38 -0700

    Add relationship-map

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -42,6 +42,8 @@ var AppView = Backbone.View.extend({
   },
 
   render: function() {
+    //console.log('this', this);
+    //console.log('this.$(#movies): ', this.$('#movies'));
     new MoviesView({
       el: this.$('#movies'),
       collection: this.collection
